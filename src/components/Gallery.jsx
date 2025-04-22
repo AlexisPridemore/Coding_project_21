b69ebd729a4986aadaa496e613d07269a0dd8039
Alexis Pridemore <alpridemore@usf.edu>
2025-04-22 13:52:20 -0400

    Fixing Gallery.jsx

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,20 +1,14 @@
 import React from 'react';
 import TourCard from './TourCard';
 
-const tours = [
-    { id: 1, name: 'Beach Paradise', location: 'Hawaii', price: '$2000' },
-    { id: 2, name: 'Mountain Adventure', location: 'Colorado', price: '$1500' },
-    { id: 3, name: 'City Lights', location: 'New York', price: '$1800' },
-];
-
-const Gallery = () => {
+const Gallery = ({ tours, onRemove }) => {
     return (
-        <div className="gallery">
-            {tours.map((tour) => (
-                <TourCard key={tour.id} tour={tour} />
-            ))}
-        </div>
+      <div className="gallery">
+        {tours.map((tour) => (
+          <TourCard key={tour.id} tour={tour} onRemove={onRemove} />
+        ))}
+      </div>
     );
-};
-
-export default Gallery;
+  };
+  
+  export default Gallery; 
